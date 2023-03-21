fa4b88ce74ae0a498c5aeef5b189ef1483629265
Júlio César <julimcvss@gmail.com>
2023-03-21 12:35:00 -0300

    modeling driver and university
    Modeling driver and university entities

---
+++ src/main/java/com/si/udriveservice/model/entity/Driver.java
@@ -0,0 +1,60 @@
+package com.si.udriveservice.model.entity;
+
+import com.si.udriveservice.model.enums.StatusEnum;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "TB_DRIVER")
+public class Driver implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private static final String SEQ_DRIVER = "SEQ_DRIVER";
+
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = SEQ_DRIVER)
+    @SequenceGenerator(name = SEQ_DRIVER, sequenceName = SEQ_DRIVER, allocationSize = 1)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
+
+    @Column(name = "cnh", nullable = false)
+    private String cnh;
+
+    @Column(name = "registration_number", nullable = false)
+    private String registrationNumber;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", nullable = false)
+    private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private StatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "university_id", nullable = false, referencedColumnName = "id")
+    private University university;
+}
